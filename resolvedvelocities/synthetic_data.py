db6579b9aa96e227b4e5554c1a7556a5ec1f92c5
Leslie Lamarche <leslie.lamarche@sri.com>
2019-05-28 09:53:40 -0700

    Completed Field object.

--- resolvedvelocities/synthetic_data.py
+++ resolvedvelocities/synthetic_data.py
@@ -1,37 +1,56 @@
 import numpy as np
 from apexpy import Apex
+from scipy import interpolate
+import coord_convert as cc
 
 
-def map_velocity_field(grid, field, alt_in):
-    # input: apex grid at altitude alt_in
-    # geodetic components of velocity on grid
-    # output: 3D geodetic latitude, longitude, altitude
-    # 3D grid of geodetic velocity components
+class Field(object):
+    def __init__(self, lat, lon, field, alt):
 
-    # define output altitudes
-    altitude = np.arange(50., 1000., 50.)
+        self.map_velocity_field([lat, lon], field, alt)
 
-    A = Apex(date=2019)
+        self.convert_to_ECEF()
+        self.create_interpolators()
 
-    field_out = []
-    lat_out = []
-    lon_out = []
-    alt_out = []
-    fs = field.shape
-    for a in altitude:
 
-        full_field = A.map_V_to_height(grid[0].flatten(), grid[1].flatten(), alt_in, a, field.reshape((fs[0]*fs[1],fs[2])).T)
-        field_out.append(full_field.T.reshape((fs[0],fs[1],fs[2])))
+    def map_velocity_field(self, grid, field, alt_in):
+        # input: apex grid at altitude alt_in
+        # geodetic components of velocity on grid
+        # output: 3D geodetic latitude, longitude, altitude
+        # 3D grid of geodetic velocity components
 
-        lat, lon, __ = A.apex2geo(grid[0],grid[1],a)
-        lat_out.append(lat)
-        lon_out.append(lon)
-        alt_out.append(np.full(lat.shape,a))
+        # define output altitudes
+        altitude = np.arange(50., 1000., 50.)
 
-    field_out = np.array(field_out)
-    lat_out = np.array(lat_out)
-    lon_out = np.array(lon_out)
-    alt_out = np.array(alt_out)
+        A = Apex(date=2019)
 
-    return lat_out, lon_out, alt_out, field_out
+        field_out = []
+        lat_out = []
+        lon_out = []
+        alt_out = []
+        fs = field.shape
+        for a in altitude:
 
+            full_field = A.map_V_to_height(grid[0].flatten(), grid[1].flatten(), alt_in, a, field.reshape((fs[0]*fs[1],fs[2])).T)
+            field_out.append(full_field.T)
+
+            lat, lon, __ = A.apex2geo(grid[0].flatten(),grid[1].flatten(),a)
+            lat_out.append(lat)
+            lon_out.append(lon)
+            alt_out.append(np.full(lat.shape,a))
+
+        self.field = np.array(field_out).reshape((fs[0]*fs[1]*len(altitude),fs[2]))
+        self.latitude = np.array(lat_out).flatten()
+        self.longitude = np.array(lon_out).flatten()
+        self.altitude = np.array(alt_out).flatten()
+
+    def convert_to_ECEF(self):
+
+        self.X, self.Y, self.Z = cc.geodetic_to_cartesian(self.latitude, self.longitude, self.altitude)
+        self.Vx, self.Vy, self.Vz = cc.vector_geodetic_to_cartesian(self.field[:,0], self.field[:,1], self.field[:,2], self.latitude, self.longitude, self.altitude)
+
+    def create_interpolators(self):
+
+        self.interpVx = interpolate.LinearNDInterpolator(np.array([self.X, self.Y, self.Z]).T, self.Vx)
+        self.interpVy = interpolate.LinearNDInterpolator(np.array([self.X, self.Y, self.Z]).T, self.Vy)
+        self.interpVz = interpolate.LinearNDInterpolator(np.array([self.X, self.Y, self.Z]).T, self.Vz)
--- resolvedvelocities/tests/test_synthetic_data.py
+++ resolvedvelocities/tests/test_synthetic_data.py
@@ -1,14 +1,16 @@
 import sys
 import os
 sys.path.append(os.path.abspath('../'))
-import synthetic_data as synth
+# import synthetic_data as synth
+from synthetic_data import Field
 import numpy as np
 
 def main():
-    synth_grid = np.meshgrid(np.linspace(63.,69.,50), np.linspace(265.,270.,50))
-    velocity = np.tile(np.array([300.,0.,0.]), (50,50,1))
+    synth_grid = np.meshgrid(np.linspace(63.,69.,10), np.linspace(265.,270.,10))
+    velocity = np.tile(np.array([300.,0.,0.]), (10,10,1))
 
-    synth.map_velocity_field(synth_grid, velocity, 300.)
+    synth_field = Field(synth_grid[0], synth_grid[1], velocity, 300.)
+
 
 if __name__ == '__main__':
     main()
