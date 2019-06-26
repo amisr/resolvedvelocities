e17d29b1af09fde0f751b84a807050e7e79735d4
Leslie Lamarche <leslie.lamarche@sri.com>
2019-06-26 09:06:47 -0700

    Modified plots so multiple figures are plotted each covering 24 hours at most.

--- resolvedvelocities/plot.py
+++ resolvedvelocities/plot.py
@@ -7,19 +7,6 @@ import datetime as dt
 
 def plot_components(utime, mlat, mlon, vector, covariance, param='V', titles=None, clim=None, cmap=None):
 
-    # pad time gaps
-    utime, [vector, covariance] = timegaps(utime, [vector, covariance])
-
-    # get x-axis (time) tick locations and labels
-    time, xticks = get_time_ticks(utime)
-
-    # get y-axis (bin) tick locations and labels
-    yticks = np.arange(len(mlat))+0.5
-    binloc = ['({} N,\n {} E)'.format(lat, lon) for lat, lon in zip(mlat, mlon)]
-
-    xedge = np.append(utime[:,0],utime[-1,1])
-    yedge = np.arange(len(mlat)+1)
-
     # set defaults
     defaults = {'V': {'titles':[['Ve1 (m/s)', 'Ve2 (m/s)', 'Ve3 (m/s) x 10'],
                                 ['errVe1 (m/s)', 'errVe2 (m/s)', 'errVe3 (m/s) x 10']],
@@ -43,50 +30,53 @@ def plot_components(utime, mlat, mlon, vector, covariance, param='V', titles=Non
         vector = vector*1000.
         covariance = covariance*1000.*1000.
 
-    fig = plt.figure(figsize=(15,10))
-    gs = gridspec.GridSpec(2,3)
-    gs.update(wspace=0.3,hspace=0.2,left=0.05,right=0.9,bottom=0.05,top=0.95)
-
-    for i, A in enumerate([vector,np.sqrt(np.diagonal(covariance,axis1=-1,axis2=-2))]):
-    # for i, A in enumerate([vector]):
-        for j in range(3):
-            # for along B component, multiply by 10
-            if j==2:
-                fac=10.
-            else:
-                fac=1.
-
-            ax = plt.subplot(gs[i,j])
-            f = ax.pcolormesh(xedge, yedge, A[:,:,j].T*fac, vmin=clim[i][0], vmax=clim[i][1], cmap=plt.get_cmap(cmap[i]))
-            ax.set_xticks(xticks)
-            ax.set_xticklabels(time)
-            ax.set_yticks(yticks)
-            ax.set_yticklabels(binloc)
-            ax.set_title(titles[i][j])
-            ax.tick_params(labelsize=8)
-
-        pos = ax.get_position()
-        cax = fig.add_axes([0.91, pos.y0, 0.015, pos.y1-pos.y0])
-        cbar = fig.colorbar(f, cax=cax)
-
-    plt.savefig('{}components.png'.format(param))
-    plt.show()
-
-
-def plot_magnitude(utime, mlat, mlon, vmag, dvmag, vdir, dvdir, param='V', titles=None, clim=None, cmap=None):
-
     # pad time gaps
-    utime, [vmag, dvmag, vdir, dvdir] = timegaps(utime, [vmag, dvmag, vdir, dvdir])
+    utime, [vector, covariance] = timegaps(utime, [vector, covariance])
 
     # get x-axis (time) tick locations and labels
-    time, xticks = get_time_ticks(utime)
+    time, xticks, xlims = get_time_ticks(utime)
 
     # get y-axis (bin) tick locations and labels
     yticks = np.arange(len(mlat))+0.5
-    binloc = ['({} N, {} E)'.format(lat, lon) for lat, lon in zip(mlat, mlon)]
+    binloc = ['({} N,\n {} E)'.format(lat, lon) for lat, lon in zip(mlat, mlon)]
 
     xedge = np.append(utime[:,0],utime[-1,1])
     yedge = np.arange(len(mlat)+1)
+
+    # loop over every day of experiment
+    for xlim in xlims:
+
+        fig = plt.figure(figsize=(15,10))
+        gs = gridspec.GridSpec(2,3)
+        gs.update(wspace=0.3,hspace=0.2,left=0.05,right=0.9,bottom=0.05,top=0.95)
+
+        for i, A in enumerate([vector,np.sqrt(np.diagonal(covariance,axis1=-1,axis2=-2))]):
+            for j in range(3):
+                # for along B component, multiply by 10
+                if j==2:
+                    fac=10.
+                else:
+                    fac=1.
+
+                ax = plt.subplot(gs[i,j])
+                f = ax.pcolormesh(xedge, yedge, A[:,:,j].T*fac, vmin=clim[i][0], vmax=clim[i][1], cmap=plt.get_cmap(cmap[i]))
+                ax.set_xticks(xticks)
+                ax.set_xticklabels(time)
+                ax.set_yticks(yticks)
+                ax.set_yticklabels(binloc)
+                ax.set_xlim(xlim)
+                ax.set_title(titles[i][j])
+                ax.tick_params(labelsize=8)
+
+            pos = ax.get_position()
+            cax = fig.add_axes([0.91, pos.y0, 0.015, pos.y1-pos.y0])
+            cbar = fig.colorbar(f, cax=cax)
+
+        plt.savefig('{}components.png'.format(param))
+        plt.show()
+
+
+def plot_magnitude(utime, mlat, mlon, vmag, dvmag, vdir, dvdir, param='V', titles=None, clim=None, cmap=None):
 
     # set defaults
     defaults = {'V': {'titles':['V magnitude (m/s)', 'V magnitude error (m/s)', 'V direction (deg)', 'V direction error (deg)', ''],
@@ -109,44 +99,63 @@ def plot_magnitude(utime, mlat, mlon, vmag, dvmag, vdir, dvdir, param='V', title
         vmag = vmag*1000.
         dvmag = dvmag*1000.
 
-    fig = plt.figure(figsize=(10,10))
-    gs = gridspec.GridSpec(5,1)
-    gs.update(hspace=0.4,left=0.15,right=0.9,bottom=0.05,top=0.95)
+    # pad time gaps
+    utime, [vmag, dvmag, vdir, dvdir] = timegaps(utime, [vmag, dvmag, vdir, dvdir])
 
-    for i, A in enumerate([vmag, dvmag, vdir, dvdir]):
-        ax = plt.subplot(gs[i])
-        f = ax.pcolormesh(xedge, yedge, A.T, vmin=clim[i][0], vmax=clim[i][1], cmap=plt.get_cmap(cmap[i]))
+    # get x-axis (time) tick locations and labels
+    time, xticks, xlims = get_time_ticks(utime)
+
+    # get y-axis (bin) tick locations and labels
+    yticks = np.arange(len(mlat))+0.5
+    binloc = ['({} N, {} E)'.format(lat, lon) for lat, lon in zip(mlat, mlon)]
+
+    xedge = np.append(utime[:,0],utime[-1,1])
+    yedge = np.arange(len(mlat)+1)
+
+    # loop over every day of experiment
+    for xlim in xlims:
+
+        fig = plt.figure(figsize=(10,10))
+        gs = gridspec.GridSpec(5,1)
+        gs.update(hspace=0.4,left=0.15,right=0.9,bottom=0.05,top=0.95)
+
+        for i, A in enumerate([vmag, dvmag, vdir, dvdir]):
+            ax = plt.subplot(gs[i])
+            f = ax.pcolormesh(xedge, yedge, A.T, vmin=clim[i][0], vmax=clim[i][1], cmap=plt.get_cmap(cmap[i]))
+            ax.set_xticks(xticks)
+            ax.set_xticklabels(time)
+            ax.set_yticks(yticks)
+            ax.set_yticklabels(binloc)
+            ax.set_xlim(xlim)
+            ax.set_title(titles[i])
+            pos = ax.get_position()
+            cax = fig.add_axes([0.91, pos.y0, 0.015, pos.y1-pos.y0])
+            cbar = fig.colorbar(f, cax=cax)
+
+        ax = plt.subplot(gs[4])
+        f = ax.quiver(xedge[:-1], yedge[:-1], vmag.T*np.sin(vdir.T*np.pi/180.), vmag.T*np.cos(vdir.T*np.pi/180.), np.sin(vdir.T*np.pi/180.), cmap=plt.get_cmap('coolwarm'))
         ax.set_xticks(xticks)
         ax.set_xticklabels(time)
-        ax.set_yticks(yticks)
+        ax.set_yticks(np.arange(len(mlat)))
         ax.set_yticklabels(binloc)
-        ax.set_title(titles[i])
+        ax.set_xlim(xlim)
         pos = ax.get_position()
         cax = fig.add_axes([0.91, pos.y0, 0.015, pos.y1-pos.y0])
-        cbar = fig.colorbar(f, cax=cax)
+        cbar = fig.colorbar(f, cax=cax, ticks=[-0.9,0,0.9])
+        cbar.ax.set_yticklabels(['West','','East'])
+        cbar.ax.quiverkey(f, 1.05, 1.1, clim[0][1], str(clim[0][1]))
 
-    ax = plt.subplot(gs[4])
-    f = ax.quiver(xedge[:-1], yedge[:-1], vmag.T*np.sin(vdir.T*np.pi/180.), vmag.T*np.cos(vdir.T*np.pi/180.), np.sin(vdir.T*np.pi/180.), cmap=plt.get_cmap('coolwarm'))
-    ax.set_xticks(xticks)
-    ax.set_xticklabels(time)
-    ax.set_yticks(np.arange(len(mlat)))
-    ax.set_yticklabels(binloc)
-    pos = ax.get_position()
-    cax = fig.add_axes([0.91, pos.y0, 0.015, pos.y1-pos.y0])
-    cbar = fig.colorbar(f, cax=cax, ticks=[-0.9,0,0.9])
-    cbar.ax.set_yticklabels(['West','','East'])
-    cbar.ax.quiverkey(f, 1.05, 1.1, clim[0][1], str(clim[0][1]))
-
-    plt.savefig('{}magnitude.png'.format(param))
-    plt.show()
+        plt.savefig('{}magnitude.png'.format(param))
+        plt.show()
 
 
 def get_time_ticks(utime):
     # get x-axis (time) tick locations and labels
-    # calculate datetime objects from unix time array
-    time = [dt.datetime.utcfromtimestamp(np.mean(t)) for t in utime]
+
     # calculate experiment length
-    exp_len = (time[-1] - time[0]).total_seconds()
+    exp_start = dt.datetime.utcfromtimestamp(utime[0,0])
+    exp_end = dt.datetime.utcfromtimestamp(utime[-1,1])
+    exp_len = (exp_end-exp_start).total_seconds()
     # set spaceing between each time tick based on lenght of experiment
     if exp_len < 1.*60.*60.:
         dtick = 10.
@@ -154,17 +163,19 @@ def get_time_ticks(utime):
         dtick = 15.
     elif exp_len < 6.*60.*60.:
         dtick = 30.
-    else:
+    elif exp_len < 12.*60.*60.:
         dtick = 60.
+    else:
+        dtick = 3*60.
 
     # get first time tick
-    h0 = time[0].replace(minute=0, second=0)
-    while h0<time[0]:
+    h0 = exp_start.replace(minute=0, second=0)
+    while h0<exp_start:
         h0 = h0+dt.timedelta(minutes=dtick)
 
     # get list of all time ticks
     time_ticks = []
-    while h0 < time[-1]:
+    while h0 < exp_end:
         time_ticks.append(h0)
         h0 = h0+dt.timedelta(minutes=dtick)
 
@@ -173,7 +184,24 @@ def get_time_ticks(utime):
     # form list of strings for actual labels
     time = [t.strftime('%H:%M') for t in time_ticks]
 
-    return time, xticks
+    xlims = []
+    if exp_len > 24.*60.*60:
+    # if experiment is longer than 1 day, break up plot limits and create a series of plots
+        st = exp_start
+        et = exp_start.replace(hour=0, minute=0, second=0) + dt.timedelta(days=1)
+        while et < exp_end:
+            xlims.append([st,et])
+            st = et
+            et = et + dt.timedelta(days=1)
+        xlims.append([st,exp_end])
+    else:
+    # if experiment shorter than one day, just create one plot
+        xlims.append([exp_start, exp_end])
+
+    # convert xlims to unix time
+    xlims = [[(x[0]-dt.datetime.utcfromtimestamp(0)).total_seconds(), (x[1]-dt.datetime.utcfromtimestamp(0)).total_seconds()] for x in xlims]
+
+    return time, xticks, xlims
 
 
 def timegaps(time, data_arrays):
